16f83986cfdcc53e5d6280430ffc989d3edc8ac1
RANA PRATAP SINGH <103735476+pratap-07R@users.noreply.github.com>
2025-01-08 16:55:41 +0530

    Add files via upload

---
+++ day2/datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3);  // we are using node js not , web browser so this code throw error
+
+
+// console.log(3+
+//     3); // this code will run in node js and it will print 6 in console, but , but code readablity should be high 
+// );
+
+
+let name = "Pratap singh";
+let age = 19;
+let isLoggedIn = false;
+let state;
+
+
+
+// number => 2 to the power 53
+// string => alsways written in double or single qoutes
+// boolean => true or false
+// null => empty value
+// undefined => value is not assigned to variable
+// symbol => unique
+
+// object => collection of key value pairs
+// array => collection of values
+// function => block of code which can be called multiple times
+
+console.log(typeof (state));  //undefined
+console.log(typeof null);  //string
