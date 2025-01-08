1857bd916e79755c2d61ac5bf4fdd5aed58b89b7
RANA PRATAP SINGH <103735476+pratap-07R@users.noreply.github.com>
2025-01-08 17:45:09 +0530

    Add files via upload

---
+++ day2/conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33;
+let score2 = "33abc"
+let score3 = null;
+let score4 = undefined;
+let score5 = true;
+
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let value = Number(score2); // this value is special type of value that is called (Not A Number/NaN)
+console.log(value);
+
+console.log(score3);
+console.log(score4);
+console.log(score5);
+
+// "33" => here 33 is as string 
+// "33abc" => NaN not a number
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// for conversion in one data type to another we use line no 22 method 
+//like we use Boolean not boolean
+// string to boolean => Boolean(string)
+// number to boolean => Boolean(number)
+// object to boolean => Boolean(object)
+// array to boolean => Boolean(array)
+
