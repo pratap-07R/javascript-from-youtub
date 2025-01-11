88e3f980523eb5d47e98840aac818a3199e6fada
RANA PRATAP SINGH <103735476+pratap-07R@users.noreply.github.com>
2025-01-11 11:09:59 +0000

    string completed

---
+++ day4/01_string.js
@@ -0,0 +1,33 @@
+"use strict" 
+
+let name = "Pratap "
+let gitRepo = 16
+
+// console.log(name + "have " + gitRepo + " repo");
+// console.log(`Hello This is ${name} and my Repo count is ${gitRepo}`); // better
+
+const gameName = new String('pratApwOrld')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__); // object 
+
+
+
+// console.log(gameName);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("A"));
+
+
+
+// Some methods => __proto__, .toUpperCase, .length, .toLowerCase, .charAt
+// .indexOf, .includes, .startsWith, .endsWith, .concat, .slice, .
+// .split, .join, .replace, .trim, .trimStart, .trimEnd
+// .repeat, .padStart, .padEnd, .localeCompare, .startsWith, .
+// .endsWith, .includes, .repeat, .padStart, .padEnd, .locale
+// .split, .join, .replace, .trim, .trimStart, .trimEnd
+// console.log(gameName.includes("A"));
+// console.log(gameName.startsWith("p"));  //true
+// console.log(gameName.endsWith("d"));     // true 
+// console.log(gameName.includes("A"));  // true
+console.log(gameName.includes("a"));    // false
