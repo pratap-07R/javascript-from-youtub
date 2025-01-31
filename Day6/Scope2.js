38a09fedbbb269d1fab31809a79c95616585aeb2
RANA PRATAP SINGH <103735476+pratap-07R@users.noreply.github.com>
2025-01-31 18:17:36 +0530

    Add files via upload

---
+++ Day6/Scope2.js
@@ -0,0 +1,21 @@
+// "use scrict"
+
+function one (){
+    const username ="Pratap"
+
+    console.log(username);
+    
+
+   function two(){
+    const website = "youtube"
+    console.log(username);
+    
+   }
+//    console.log(website);   //we can not access inline variable  
+
+
+   two()
+   
+    
+}
+one()
