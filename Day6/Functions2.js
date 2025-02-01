dc8517228cfdb624beb2cfc452d395be96ed9037
RANA PRATAP SINGH <103735476+pratap-07R@users.noreply.github.com>
2025-02-01 18:24:30 +0530

    Add files via upload
    javascript function part 2

---
+++ Day6/Functions2.js
@@ -0,0 +1,16 @@
+"use strict"
+
+function calculateCartPrice( ...num1){ //without using spered
+    return num1
+
+}
+// console.log(calculateCartPrice(1000,200 ,4000)); // Output: 10
+
+const user = {
+    username : "pratap",
+    price: 199,
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
